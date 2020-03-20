306b78138dc781fc15d9f8e987014424dbffeae7
Vadym Kasian <vadym.kasian1@gmail.com>
2020-03-20 16:11:15 +0200

    fix

--- Tests.java
+++ Tests.java
@@ -2,37 +2,43 @@ package com.ChromeWebSearch;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Tests {
+    ChromeDriver driver;
+    @BeforeTest
+    public void setup(){
+        System.setProperty("webdriver.chrome.driver","D:/java/programs/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
 
     @Test
     public void testBrowser(){
         String searchQuery = "test";
 
-        System.setProperty("webdriver.chrome.driver","D:/java/programs/chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
         driver.get("http://google.com.ua/");
         BrowserSearch browser = new BrowserSearch(driver);
         browser.browserSearch(searchQuery);
 
         Assert.assertEquals(browser.getTitle(), searchQuery + " - Google Search");
-
-        driver.close();
     }
 
     @Test
     public void testBrowserWithRuby(){
         String searchQuery = "Ruby";
 
-        System.setProperty("webdriver.chrome.driver","D:/java/programs/chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
         driver.get("http://google.com.ua/");
         BrowserSearch browser = new BrowserSearch(driver);
         browser.browserSearch(searchQuery);
 
         Assert.assertEquals(browser.getTitle(), searchQuery + " - Google Search");
+    }
 
+    @AfterTest
+    public void closeConnections(){
         driver.close();
     }
 }
+
