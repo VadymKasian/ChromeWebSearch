d806b55ca30a27792a15bfd50c7891102d92db4f
VadymKasian <62418694+VadymKasian@users.noreply.github.com>
2020-03-20 11:41:13 +0200

    switch to page factory

--- BrowserSearch.java
+++ BrowserSearch.java
@@ -1,29 +1,52 @@
 package com.testChrome;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+
 public class BrowserSearch {
     String searchQuery = "test";
+    WebDriver driver;
 
     @Test
     public void testBrowser(){
         System.setProperty("webdriver.chrome.driver","D:/java/selenium/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-
+        driver = new ChromeDriver();
         driver.get("http://google.com.ua/");
+        Browser browser = new Browser(driver);
+        browser.browserSearch(searchQuery);
 
-        WebElement inputTypeSearch = driver.findElement(By.name("q"));
-        inputTypeSearch.sendKeys(searchQuery);
-        inputTypeSearch.sendKeys(Keys.ENTER);
+        Assert.assertEquals(browser.getTitle(), searchQuery + " - Google Search");
 
-        Assert.assertEquals(driver.getTitle(), searchQuery + " - Google Search");
+        driver.close();
+    }
+}
 
-        //driver.close();
+class Browser{
+    WebDriver driver;
+
+    String pageTitle;
+    @FindBy(name = "q")
+    private WebElement searchInput;
+
+    Browser(WebDriver driver){
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    public void browserSearch(String text){
+        searchInput.sendKeys(text);
+        searchInput.sendKeys(Keys.ENTER);
+        pageTitle = text + " - Google Search";
+    }
+
+    public String getTitle(){
+        return pageTitle;
     }
 }
